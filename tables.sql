c5d0874073e3de746163c7fa3d4dbebe0dc15935
Wessias <viggowessner@gmail.com>
2025-01-28 20:39:15 +0100

    missed a foreign key

--- tables.sql
+++ tables.sql
@@ -61,7 +61,7 @@ CREATE TABLE MandatoryBranch(
     course CHAR(6) NOT NULL,
     branch TEXT NOT NULL,
     program TEXT NOT NULL,
-    PRIMARY KEY (course, branch, program),
+    PRIMARY KEY course REFERENCES Courses(code),
     FOREIGN KEY (branch,program) REFERENCES Branches(name, program)
 );
 
